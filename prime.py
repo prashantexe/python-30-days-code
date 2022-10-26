bdd7807d9a5586b73c25608011a4da96c09f7311
prashantexe <prashantd049@gmail.com>
2022-10-26 21:57:15 +0530

    prime number

---
+++ prime.py
@@ -0,0 +1,20 @@
+num = int(input("Enter the number to find prime or not!! :"))
+
+# define a variable (flag)
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
