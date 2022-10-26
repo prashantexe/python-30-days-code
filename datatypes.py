6fb68af8f1f51ecd07040730a90b9abd065b8188
prashantexe <prashantd049@gmail.com>
2022-10-26 21:51:06 +0530

    Datatypes

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+i = 4
+d = 4.0
+s = 'Python'
+
+# Declare second int,double,str
+i2 = None
+d2 = None
+s2 = None
+
+# Read and save an int,double,str
+i2 = int(input())
+d2 = float(input())
+s2 = str(input())
+
+# Print the sum of both integer variables
+print(i + i2)
+
+# Print the sum of the double variables
+print(d + d2)
+
+# Concatenate and print the String variables
+print(s + s2)
