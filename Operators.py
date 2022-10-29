afd12c675a0517168779a0658d3cee0e6191f34f
prashantexe <prashantd049@gmail.com>
2022-10-29 20:07:27 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,17 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def solve(meal_cost, tip_percent, tax_percent):
+
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
