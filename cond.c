b488b2179ba1e27818539bbbb11b6e457a9bdbf0
prashantexe <prashantd049@gmail.com>
2022-10-31 21:20:00 +0530

    Conditionall

---
+++ cond.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    if(n%2==1 || (n%2==0 && n>=6 && n<=20))
+    printf("Weird\n");
+    else if(n%2==0 && ( (n>=2 && n<=5) || n>20))
+    printf("Not Weird\n");
+
+}
